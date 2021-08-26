64aa3236526dbb95301f7c798ff692a7234fb0f2
Leandro Leao <leandro.leao@outlook.com>
2021-08-26 18:32:37 -0300

    resoluçao dos exercicios de maneira simples do 01 ao 04

---
+++ 01.fundamentos/04.Introducao-a-javascript-e-logica-de-programacao/Dia02.Javascript-array-e-loop-for/exercicio01-a-04.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somaNumber = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+let media = somaNumber / numbers.length;
+
+if (media > 20) {
+  console.log ("Valor maior que 20");
+} else {
+  console.log ("Valor menor ou igual a 20");
+}
+
+
