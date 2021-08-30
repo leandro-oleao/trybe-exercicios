a8067a1cadb280ddd7a5a32866580d6c78f3e682
Leandro Leao <leandro.leao@outlook.com>
2021-08-30 18:35:05 -0300

    inclusao do exercicio parte 01

---
+++ 01.fundamentos/04.Introducao-a-javascript-e-logica-de-programacao/exercicio-parte01.js
@@ -0,0 +1,23 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+let info02 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info && info02) {
+    if (info[key] === info02[key]) {
+    console.log('Ambos recorrentes');
+    } else {
+      console.log(info[key] + ' e ' + info02[key]);
+    }
+    
+}
+
