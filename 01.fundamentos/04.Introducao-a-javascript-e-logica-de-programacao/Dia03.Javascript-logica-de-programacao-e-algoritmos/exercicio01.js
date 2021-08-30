b4e90f4fc4e0431a63a4ab0f8b69741d687273c4
Leandro Leao <leandro.leao@outlook.com>
2021-08-30 18:11:58 -0300

    inclusão de exercicios dia 03

---
+++ 01.fundamentos/04.Introducao-a-javascript-e-logica-de-programacao/Dia03.Javascript-logica-de-programacao-e-algoritmos/exercicio01.js
@@ -0,0 +1,12 @@
+let numbers = 10;
+
+let asterisco = '*'
+let totalAsterisco = '';
+
+for (let index = 0; index < numbers; index +=1) {
+totalAsterisco = totalAsterisco + asterisco;
+}
+
+for (let index = 0; index < numbers; index +=1) {
+  console.log(totalAsterisco);
+}
---
+++ 01.fundamentos/04.Introducao-a-javascript-e-logica-de-programacao/Dia03.Javascript-logica-de-programacao-e-algoritmos/exercicio02.js
@@ -0,0 +1,12 @@
+let numbers = 5;
+
+let asterisco = '*'
+let totalAsterisco = '';
+
+for (let index = 0; index < numbers; index +=1) {
+ totalAsterisco = totalAsterisco + asterisco;
+ totalAsterisco.push();
+}
+
+
+console.log(totalAsterisco);
